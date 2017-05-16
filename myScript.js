ef24c4570dae92385d7fe0cd8236f38ad0002c05
djedje <thedjedje@hotmail.fr>
2017-05-16 22:40:49 +0200

    new commit !

--- myScript.js
+++ myScript.js
@@ -1,11 +1,4 @@
-var myVar = 0; 
-function myFunction() {
-    console.log("test !");
-    myVar++;
-    console.log("j'ai cliqué "+ myVar +" fois !");
-}
-
-function myFunction2(monElement) {
-    var toto = document.getElementById("mySpan");
-    toto.innerHTML = monElement.value;
-}
+var app = angular.module('craApp', []);
+app.controller('myCtrl', function($scope) {
+    
+});
